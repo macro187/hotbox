4f22e6df83d53bad70c9353ea3d819616d84f698
Ron MacNeil <macro@hotmail.com>
2023-03-19 16:46:22 +0900

    Enable specs to specify shell argument

--- lib/source-spec.sh
+++ lib/source-spec.sh
@@ -3,17 +3,22 @@ test -f $HOTBOX/specs/$HOTBOX_SPEC.sh || die "Spec $HOTBOX_SPEC not found"
 
 _baseimage="${HOTBOX_BASEIMAGE:-}"
 _features="${HOTBOX_FEATURES:-}"
-_shell="${HOTBOX_SHELL:-}"
 _workdir="${HOTBOX_WORKDIR:-}"
+_shell="${HOTBOX_SHELL:-}"
+_shellarg="${HOTBOX_SHELLARG:-}"
 
 HOTBOX_BASEIMAGE=""
 HOTBOX_FEATURES=""
-HOTBOX_SHELL=""
 HOTBOX_WORKDIR=""
+HOTBOX_SHELL=""
+HOTBOX_SHELLARG=""
 . $HOTBOX/specs/$HOTBOX_SPEC.sh
 
 
 HOTBOX_BASEIMAGE="${_baseimage:-$HOTBOX_BASEIMAGE}"
 HOTBOX_FEATURES="${HOTBOX_FEATURES}${_features:+ $_features}"
-HOTBOX_SHELL="${_shell:-$HOTBOX_SHELL}"
 HOTBOX_WORKDIR="${_workdir:-$HOTBOX_WORKDIR}"
+if [ -n "$_shell" ] ; then
+    HOTBOX_SHELL="$_shell"
+    HOTBOX_SHELLARG="$_shellarg"
+fi
