c6e53b69a8e096c6cede4ddb03cc9562d330637f
Ron MacNeil <macro@hotmail.com>
2023-04-24 11:51:43 +0900

    Add bash-language-server feature

---
+++ features/bash-language-server.setup.feature.sh
@@ -0,0 +1,33 @@
+. $HOTBOX/lib/sh.sh
+
+
+if ! which bash-language-server >/dev/null ; then
+    echo_on
+    doas npm i -g bash-language-server
+    echo_off
+fi
+
+
+if ! which shellcheck >/dev/null ; then
+    heading "Installing shellcheck"
+    case $(current_distro) in
+
+        alpine)
+            echo_on
+            doas apk add shellcheck shellcheck-doc
+            echo_off
+            ;;
+
+        ubuntu)
+            echo_on
+            doas \
+                env DEBIAN_FRONTEND=noninteractive \
+                apt install -y --no-install-recommends shellcheck
+            echo_off
+            ;;
+
+        *)
+            die "Don't know how to install shellcheck on $current_distro os"
+            ;;
+    esac
+fi
