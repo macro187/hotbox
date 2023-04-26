83875d794ae8d8a71f97db7c5c87f4d7a22d313b
Ron MacNeil <macro@hotmail.com>
2023-04-26 23:55:26 +0900

    Build containers long-hand
    Using docker create/start instead of a Dockerfile + docker build, which seems
    simpler and removes restrictions like not being able to bind mount from the
    host.

--- features/container-hotbox.setup.feature.sh
+++ features/container-hotbox.setup.feature.sh
@@ -9,12 +9,3 @@ echo_on
 cd $HOME
 echo 'PATH="/hotbox:$PATH"' >>.profile
 echo_off
-
-
-heading "Creating hotbox state mount point"
-echo_on
-doas rm -rf /hotbox-state
-doas mkdir /hotbox-state
-doas chown $user:$user /hotbox-state
-doas chmod 755 /hotbox-state
-echo_off
