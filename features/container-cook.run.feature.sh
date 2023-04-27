610e8ec54c5f9f37386a08439359fb625b7bf3f9
Ron MacNeil <macro@hotmail.com>
2023-04-28 00:42:08 +0900

    container-cook: Warn if cook not found and won't be available

--- features/container-cook.run.feature.sh
+++ features/container-cook.run.feature.sh
@@ -1,18 +1,28 @@
 . $HOTBOX/lib/sh.sh
 
 
-cook="$(which cook)" || exit 0
+heading "Locating cook"
+
+
+cook="$(which cook || true)"
 cook="${cook%/*}"
+if [ -z "$cook" ] ; then
+    warn "cook not found on PATH, /cook will be unavailable in container"
+    exit 0
+fi
 
 
 host_cook="$cook"
 if $HOTBOX/hotbox-in-container ; then
     host_cook="$($HOTBOX/hotbox-map $cook)"
     if [ -z "$host_cook" ] ; then
-        warn "$cook will be unavailable because it has not been bind mounted from the host"
+        warn "$cook not bind-mounted from host, /cook will be unavailable in container"
         exit 0
     fi
 fi
+
+
+info "Found cook at $cook ($host_cook on host)"
 
 
 echo --volume $host_cook:/cook
