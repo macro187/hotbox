b48e7f6f967f28b8dcef057b98aae1ca44812cb5
Ron MacNeil <macro@hotmail.com>
2023-05-01 13:05:03 +0900

    vimrc: Configure wildmenu and wilder if available

--- features/ron-vim-plugins.setup.feature.sh
+++ features/ron-vim-plugins.setup.feature.sh
@@ -5,9 +5,10 @@ heading "Installing Ron's Vim plugins"
 echo_on
 cd ~/.vim/pack/hotbox/start
 git clone https://github.com/macro187/vim-macrobsidian.git
+git clone --depth 1 https://github.com/ryanoasis/vim-devicons.git
+git clone --depth 1 https://github.com/gelguy/wilder.nvim.git
 git clone --depth 1 https://github.com/preservim/nerdtree.git
 git clone --depth 1 https://github.com/prabirshrestha/vim-lsp.git
 git clone --depth 1 https://github.com/prabirshrestha/asyncomplete.vim.git
 git clone --depth 1 https://github.com/prabirshrestha/asyncomplete-lsp.vim.git
-git clone --depth 1 https://github.com/ryanoasis/vim-devicons.git
 echo_off
