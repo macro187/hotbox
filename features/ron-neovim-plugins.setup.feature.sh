a583996b84940aa81547bcb478927bcf922b8821
Ron MacNeil <macro@hotmail.com>
2023-05-09 10:42:23 +0900

    Put Ron's (neo)vim plugins in their own directory

--- features/ron-neovim-plugins.setup.feature.sh
+++ features/ron-neovim-plugins.setup.feature.sh
@@ -1,9 +1,19 @@
 . $HOTBOX/lib/sh.sh
 
 
+heading "Creating Ron's Neovim plugin directory"
+echo_on
+cd $HOME
+mkdir -p .local/share/nvim/site/pack
+cd .local/share/nvim/site/pack
+rm -rf ron
+mkdir -p ron/start
+echo_off
+
+
 heading "Installing Ron's Neovim plugins"
 echo_on
-cd ~/.local/share/nvim/site/pack/hotbox/start
+cd ~/.local/share/nvim/site/pack/ron/start
 git clone https://github.com/macro187/vim-macrobsidian.git
 git clone --depth 1 https://github.com/nvim-tree/nvim-web-devicons.git
 git clone --depth 1 https://github.com/nvim-tree/nvim-tree.lua.git
--- features/ron-vim-plugins.setup.feature.sh
+++ features/ron-vim-plugins.setup.feature.sh
@@ -1,9 +1,19 @@
 . $HOTBOX/lib/sh.sh
 
 
+heading "Creating Ron's Vim plugin directory"
+echo_on
+cd $HOME
+mkdir -p .vim/pack
+cd .vim/pack
+rm -rf ron
+mkdir -p ron/start
+echo_off
+
+
 heading "Installing Ron's Vim plugins"
 echo_on
-cd ~/.vim/pack/hotbox/start
+cd ~/.vim/pack/ron/start
 git clone https://github.com/macro187/vim-macrobsidian.git
 git clone --depth 1 https://github.com/ryanoasis/vim-devicons.git
 git clone --depth 1 https://github.com/gelguy/wilder.nvim.git
