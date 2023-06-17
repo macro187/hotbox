68eeafe00c31b55aeedce10d09b0ef75bd3afb2f
Ron MacNeil <macro@hotmail.com>
2023-06-17 13:15:43 +0900

    state.sh: Check assumptions more clearly

--- lib/state.sh
+++ lib/state.sh
@@ -7,9 +7,18 @@ test -n "${__hotbox_state:+x}" && return ; __hotbox_state=1
 HOTBOX_STATE="${HOTBOX_STATE:-}"
 
 
-if [ -z "$HOTBOX_STATE" ] ; then
-    ! $HOTBOX/hotbox-in-container || die "In container but HOTBOX_STATE not set"
+if $HOTBOX/hotbox-in-container && [ -z "$HOTBOX_STATE" ] ; then
+    die "In container but HOTBOX_STATE not set"
+fi
 
+
+if [ -n "$HOTBOX_STATE" ] && [ ! -d "$HOTBOX_STATE" ] ; then
+    die "Specified HOTBOX_STATE location doesn't exist: $HOTBOX_STATE"
+
+fi
+
+
+if [ -z "$HOTBOX_STATE" ] ; then
     HOTBOX_STATE="$HOME/.hotbox-state"
     if ! [ -d $HOTBOX_STATE ] ; then
         heading "Creating hotbox state directory"
