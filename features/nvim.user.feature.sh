05150c0ae9ac0a82536cbe4e0742419d1f78ea10
Ron MacNeil <macro@hotmail.com>
2023-07-01 14:18:42 +0900

    nvim: Install directly from the nvim project
    The version in apt is ancient.

--- features/nvim.user.feature.sh
+++ features/nvim.user.feature.sh
@@ -1,27 +1,53 @@
 . $HOTBOX/lib/sh.sh
+. $HOTBOX/lib/state.sh
+
+
+install_alpine() {
+    echo_on
+    doas apk add neovim neovim-doc
+    echo_off
+}
+
+
+install_ubuntu() {
+    version="0.9.1"
+    localdistfile="nvim-$version-linux64.tar.gz"
+    url="https://github.com/neovim/neovim/releases/download/v$version/nvim-linux64.tar.gz"
+
+    cd $HOME
+
+    if [ -f $HOTBOX_STATE/cache/$localdistfile ] ; then
+        heading "Copying nvim from cache"
+        echo_on
+        cp $HOTBOX_STATE/cache/$localdistfile ./
+        echo_off
+    else
+        heading "Downloading nvim"
+        echo_on
+        wget -nv -O $localdistfile "$url"
+        cp -f $localdistfile $HOTBOX_STATE/cache/
+        echo_off
+    fi
+
+    heading "Extracting nvim"
+    echo_on
+    tar xf $localdistfile
+    mv nvim-linux64 nvim
+    rm $localdistfile
+    echo_off
+
+    heading "Linking nvim to user bin/"
+    echo_on
+    cd $HOME/bin
+    ln -s $HOME/nvim/bin/nvim
+    echo_off
+}
 
 
 if ! which nvim >/dev/null ; then
-    case $(current_distro) in
-
-        alpine)
-            echo_on
-            doas apk add neovim neovim-doc
-            echo_off
-            ;;
-
-        ubuntu)
-            echo_on
-            doas \
-                env DEBIAN_FRONTEND=noninteractive \
-                apt install -y --no-install-recommends neovim
-            echo_off
-            ;;
-
-        *)
-            die "Don't know how to install vim on $(current_distro) os"
-            ;;
-    esac
+    install=install_$(current_distro)
+    function_exists $install || die "Don't know how to install nvim on $(current_distro) os"
+    $install
 fi
 
 
