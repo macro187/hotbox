43a7c8dc7f9e218bbe51551707da596f21b8ee42
Ron MacNeil <macro@hotmail.com>
2023-07-03 23:02:15 +0900

    Add cook feature

---
+++ features/cook.user.feature.sh
@@ -0,0 +1,21 @@
+. $HOTBOX/lib/sh.sh
+
+
+cd $HOME
+
+
+if [ ! -d $HOME/cook ] ; then
+    heading "Cloning cook"
+    echo_on
+    $HOTBOX/lib/git-clone-cached https://github.com/macro187/cook.git
+    echo_off
+fi
+
+
+heading "Adding cook to path"
+echo_on
+rm -f .profile.d/??-cook.sh
+cat << EOF >>.profile.d/50-cook.sh
+PATH="$HOME/cook:\$PATH"
+EOF
+echo_off
