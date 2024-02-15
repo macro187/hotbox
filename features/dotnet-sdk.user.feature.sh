8e46135a22e6c460f4bfcc7384204e4efce759c9
Ron MacNeil <macro@hotmail.com>
2024-02-16 07:24:56 +0900

    DOTNET_ROLL_FORWARD

--- features/dotnet-sdk.user.feature.sh
+++ features/dotnet-sdk.user.feature.sh
@@ -28,3 +28,13 @@ info "Shutting up dotnet welcome message"
 echo_on
 dotnet help &>/dev/null
 echo_off
+
+
+info "Writing .profile.d/50-dotnet-roll-forward.sh"
+echo_on
+rm -f .profile.d/??-dotnet-roll-forward.sh
+cat << 'EOF' >.profile.d/50-dotnet-roll-forward.sh
+    # Always run on the latest installed .NET framework
+    export DOTNET_ROLL_FORWARD="LatestMajor"
+EOF
+echo_off
