515e56cbe0d85e14a69b62dbb67601e014445eb7
Ron MacNeil <macro@hotmail.com>
2024-02-16 11:24:22 +0900

    container-user: Remove existing conflicting user if necessary

--- features/container-user.system.feature.sh
+++ features/container-user.system.feature.sh
@@ -6,6 +6,19 @@ test -n "${HOTBOX_UID:-}" || die "HOTBOX_UID not set"
 test -n "${HOTBOX_GID:-}" || die "HOTBOX_GID not set"
 
 
+existing_user="$(id -un $HOTBOX_UID || true)"
+if [ -n "$existing_user" ] ; then
+    echo_on
+    userdel -r "$existing_user"
+    echo_off
+    if [ -d /home/$existing_user ] ; then
+        echo_on
+        mv /home/$existing_user /home/$existing_user.orig
+        echo_off
+    fi
+fi
+
+
 if which adduser >/dev/null 2>&1 && which addgroup >/dev/null 2>&1 ; then
     echo_on
     addgroup --gid $HOTBOX_GID $HOTBOX_USER
@@ -15,6 +28,9 @@ elif which useradd >/dev/null 2>&1 && which groupadd >/dev/null 2>&1 ; then
     echo_on
     groupadd --gid $HOTBOX_GID $HOTBOX_USER
     useradd --uid $HOTBOX_UID -g $HOTBOX_USER $HOTBOX_USER
+    mkdir /home/$HOTBOX_USER
+    cp -rT /etc/skel /home/$HOTBOX_USER
+    chown -R $HOTBOX_USER:$HOTBOX_USER /home/$HOTBOX_USER
     echo_off
 else
     die "Neither adduser/addgroup nor useradd/groupadd are present"
