47af61a1289db214c80418440ef9c23a5ddfc1cf
Ron MacNeil <macro@hotmail.com>
2024-02-27 09:01:02 +0900

    docker-cli-compose: Support Ubuntu

--- features/docker-cli-compose.user.feature.sh
+++ features/docker-cli-compose.user.feature.sh
@@ -10,6 +10,13 @@ if ! docker compose version >/dev/null 2>&1 ; then
             echo_off
             ;;
 
+        ubuntu)
+            echo_on
+            doas env DEBIAN_FRONTEND=noninteractive \
+                apt install -y --no-install-recommends docker-compose-v2
+            echo_off
+            ;;
+
         *)
             die "Don't know how to install Docker Compose on $(current_distro) os"
             ;;
